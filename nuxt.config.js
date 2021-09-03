cd2ee97fd7a9404381707c951a1afe66bdac3bf9
juniortour <juniortour@qq.com>
2021-09-03 23:32:06 +0800

    fix: update ssr example

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
   },
 
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
