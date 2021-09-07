633ac52a7787fb416e4b35a8074a8d9c6a71b43c
juniortour <juniortour@qq.com>
2021-09-07 23:55:08 +0800

    feat: add jest test example

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js',
+  },
+  moduleFileExtensions: ['js', 'vue', 'json'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
+  },
+  globals: {
+    'vue-jest': {
+      templateCompiler: {
+        compiler: require('vue-template-babel-compiler')
+      }
+    }
+  }
+}
---
+++ test/components.spec.js
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils'
+import Tutorial from '@/components/Tutorial.vue'
+
+describe('jest test with vue-template-babel-compiler', () => {
+  test('should work for Optional Chaining', () => {
+    const wrapper = mount(Tutorial)
+    expect(wrapper.vm).toBeTruthy()
+    expect(wrapper.vm.$el.innerHTML).toMatch('Optional Chaining enabled: true')
+  })
+})
